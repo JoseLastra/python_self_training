46af767f405820362ddccb4d75ebdea5640ba9e2
JoseLastra <jose.lastra@pucv.cl>
2025-10-23 11:43:41 +0200

    until plot because of failure in python packages environment

--- geocomputation_intro.py
+++ geocomputation_intro.py
@@ -1,1 +1,25 @@
 # geocomputation intro
+import pandas as pd
+import shapely
+import geopandas as gpd
+
+## limit the number of printed rows
+pd.set_option("display.max_rows", 6)
+
+## for reading a geopackage from our path as a geodataframe (gdf)
+gdf = gpd.read_file("data/world.gpkg")
+
+# check output class
+type(gdf)
+gdf.shape  # dimensions
+
+## sf like following pandas structure so you can use pandas subset base code
+gdf = gdf[["name_long", "geometry"]]
+gdf
+
+## creating a sub dataset based on a condition
+## filtering egypt
+gdf[gdf["name_long"] == "Egypt"]
+
+# basic plot
+gdf.plot()
